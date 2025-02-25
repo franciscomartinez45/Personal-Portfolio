d9b2f0e288b4a6057ff184c00d1e03ef85c5f950
Francisco Martinez <fmartinez110@toromail.csudh.edu>
2025-02-24 21:48:39 -0800

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Menu } from "./Menu";
 import Index from "./Index";
-
 import "./style/index.css";
 import { ThemeProvider } from "./style/ThemeContext";
-
+import { Analytics } from "@vercel/analytics/react";
 export const App = () => {
   return (
     <div>
+      <Analytics />
       <ThemeProvider>
         <Menu />
         <Index />
